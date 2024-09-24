772dfff6d028c82944d117a34c071f70997867f7
Acetonerer <bolokan2014@gmail.com>
2024-09-24 12:33:31 +0700

    fix SECRET_KEY field

--- link_checker/settings.py
+++ link_checker/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = 'django-insecure-kzp3-i5_m!+z!=kw%r-fjxzkbt60fr$y-brmxnlf8tzm#we%!t'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
